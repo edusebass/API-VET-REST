0e09c867b5b79d970c805bd5664cdc7e5f43b3ce
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-08 16:36:53 -0500

    registro de usuarios completo mongodb

--- src/controllers/veterinario_controller.js
+++ src/controllers/veterinario_controller.js
@@ -1,12 +1,35 @@
+import Veterinario from "../models/Veterinario.js"
+
 const login =(req,res)=>{
     res.status(200).json({res:'login del veterinario'})
 }
 const perfil=(req,res)=>{
     res.status(200).json({res:'perfil del veterinario'})
 }
-const registro =(req,res)=>{
-    res.status(200).json({res:'registro de un nuevo veterinario'})
+
+
+//Metodo para registrar 
+const registro = async (req,res)=>{
+    //Desestructura los campos
+    const {email,password} = req.body
+    // Validar todos los campos
+    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    //Obtener el usuario de la BD en base al email
+    const verificarEmailBDD = await Veterinario.findOne({email})
+    //Validar que el email sea nuevo
+    if(verificarEmailBDD) return res.status(400).json({msg:"Lo sentimos, el email ya se encuentra registrado"})
+    //Creando la nuevva instancia del veterinario
+    const nuevoVeterinario = new Veterinario(req.body)
+    //Encryptar el password
+    nuevoVeterinario.password = await nuevoVeterinario.encrypPassword(password)
+    //Crear el token => mail
+    nuevoVeterinario.crearToken()
+    //Guarda en una BD
+    await nuevoVeterinario.save()
+    //Responder
+    res.status(200).json({nuevoVeterinario})
 }
+
 const confirmEmail = (req,res)=>{
     res.status(200).json({res:'confirmar email de registro de veterinario'})
 }
