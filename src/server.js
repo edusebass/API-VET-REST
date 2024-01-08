f47a6f791cb339a5f2bd340e7d3a057811132d4d
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-08 16:08:07 -0500

    llamado a los routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors';
+import routerVeterinarios from './routers/veterinario_routes.js'
 
 // Inicializaciones
 const app = express()
@@ -22,6 +23,12 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send("Server on")
 })
+
+app.use('/api',routerVeterinarios)
+// Manejo de una ruta que no sea encontrada
+app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
+
+
 
 // Exportar la instancia de express por medio de app
 export default  app
