faf1d6e5012fdec25c9d679aad3e6c4721c3be3c
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-15 16:12:20 -0500

    actualizacion password

--- src/controllers/veterinario_controller.js
+++ src/controllers/veterinario_controller.js
@@ -84,12 +84,39 @@ const detalleVeterinario = async(req,res)=>{
     res.status(200).json({msg:veterinarioBDD})
 }
 
-const actualizarPerfil = (req,res)=>{
-    res.status(200).json({res:'actualizar perfil de un veterinario registrado'})
+const actualizarPerfil = async (req,res)=>{
+    const {id} = req.params
+    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, debe ser un id válido`});
+    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    const veterinarioBDD = await Veterinario.findById(id)
+    if(!veterinarioBDD) return res.status(404).json({msg:`Lo sentimos, no existe el veterinario ${id}`})
+    if (veterinarioBDD.email !=  req.body.email)
+    {
+        const veterinarioBDDMail = await Veterinario.findOne({email:req.body.email})
+        if (veterinarioBDDMail)
+        {
+            return res.status(404).json({msg:`Lo sentimos, el existe ya se encuentra registrado`})  
+        }
+    }
+    veterinarioBDD.nombre = req.body.nombre || veterinarioBDD?.nombre
+    veterinarioBDD.apellido = req.body.apellido  || veterinarioBDD?.apellido
+    veterinarioBDD.direccion = req.body.direccion ||  veterinarioBDD?.direccion
+    veterinarioBDD.telefono = req.body.telefono || veterinarioBDD?.telefono
+    veterinarioBDD.email = req.body.email || veterinarioBDD?.email
+    await veterinarioBDD.save()
+    res.status(200).json({msg:"Perfil actualizado correctamente"})
 }
-const actualizarPassword = (req,res)=>{
-    res.status(200).json({res:'actualizar password de un veterinario registrado'})
+
+const actualizarPassword = async (req,res)=>{
+    const veterinarioBDD = await Veterinario.findById(req.veterinarioBDD._id)
+    if(!veterinarioBDD) return res.status(404).json({msg:`Lo sentimos, no existe el veterinario ${id}`})
+    const verificarPassword = await veterinarioBDD.matchPassword(req.body.passwordactual)
+    if(!verificarPassword) return res.status(404).json({msg:"Lo sentimos, el password actual no es el correcto"})
+    veterinarioBDD.password = await veterinarioBDD.encrypPassword(req.body.passwordnuevo)
+    await veterinarioBDD.save()
+    res.status(200).json({msg:"Password actualizado correctamente"})
 }
+
 const recuperarPassword = async(req,res)=>{
     const {email} = req.body
     if (Object.values(req.body).includes("")) return res.status(404).json({msg:"Lo sentimos, debes llenar todos los campos"})
