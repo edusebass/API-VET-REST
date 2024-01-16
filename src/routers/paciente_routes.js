1a5c4b4e6b90c7093e947573de66c03df683ab2a
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-16 15:58:46 -0500

    add controladores a las rutas

--- src/routers/paciente_routes.js
+++ src/routers/paciente_routes.js
@@ -1,21 +1,22 @@
 import {Router} from 'express'
+import {
+    actualizarPaciente,
+    detallePaciente,
+    eliminarPaciente,
+    listarPacientes,
+    registrarPaciente,
+    loginPaciente,
+    perfilPaciente 
+} from "../controllers/paciente_controller.js";
 const router = Router()
 
-//Ruta para listar todos los pacientes
-router.post('/paciente/login',(req,res)=>res.send("Login del paciente"))
 
-//Ruta para ver el detalle de un paciente en particular
-router.get('/paciente/perfil',(req,res)=>res.send("Perfil del paciente"))
-
-//Ruta para registrar un paciente
-router.get('/pacientes',(req,res)=>res.send("Listar pacientes"))
-
-//Ruta para obtener un paciente en especifico
-router.get('/paciente/:id',(req,res)=>res.send("Detalle del paciente"))
-
-
-router.post('/paciente/registro',(req,res)=>res.send("Registrar paciente"))
-router.put('/paciente/actualizar/:id',(req,res)=>res.send("Actualizar paciente"))
-router.delete('/pacientes/eliminar/:id',(req,res)=>res.send("Eliminar paciente"))
+router.post('/paciente/login',loginPaciente)
+router.get('/paciente/perfil',perfilPaciente)
+router.get('/pacientes',listarPacientes)
+router.get('/paciente/:id',detallePaciente)
+router.post('/paciente/registro',registrarPaciente)
+router.put('/paciente/actualizar/:id',actualizarPaciente)
+router.delete('/paciente/eliminar/:id',eliminarPaciente)
 
 export default router
