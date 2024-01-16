869c2c9c6cdb5c1fe311aad37971994aaea3ac7d
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-16 16:05:59 -0500

    proteccion de rutas

--- src/routers/paciente_routes.js
+++ src/routers/paciente_routes.js
@@ -8,15 +8,17 @@ import {
     loginPaciente,
     perfilPaciente 
 } from "../controllers/paciente_controller.js";
+import verificarAutenticacion from "../middlewares/autenticacion.js";
 const router = Router()
 
 
 router.post('/paciente/login',loginPaciente)
-router.get('/paciente/perfil',perfilPaciente)
-router.get('/pacientes',listarPacientes)
-router.get('/paciente/:id',detallePaciente)
-router.post('/paciente/registro',registrarPaciente)
-router.put('/paciente/actualizar/:id',actualizarPaciente)
-router.delete('/paciente/eliminar/:id',eliminarPaciente)
+
+router.get('/paciente/perfil',verificarAutenticacion,perfilPaciente)
+router.get("/pacientes",verificarAutenticacion,listarPacientes);
+router.get("/paciente/:id",verificarAutenticacion, detallePaciente);
+router.post("/paciente/registro", verificarAutenticacion,registrarPaciente);
+router.put("/paciente/actualizar/:id", verificarAutenticacion,actualizarPaciente);
+router.delete("/paciente/eliminar/:id", verificarAutenticacion,eliminarPaciente);
 
 export default router
